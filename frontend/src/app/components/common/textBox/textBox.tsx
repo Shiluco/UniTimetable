b104e65ab3e2e1b90e905a3cfdc4ca4f91025b0a
koto623 <akan.02toko@gmail.com>
2025-01-24 04:42:10 +0900

    feat: loginFormを作成

--- frontend/src/app/components/common/textBox/textBox.tsx
+++ frontend/src/app/components/common/textBox/textBox.tsx
@@ -4,10 +4,22 @@ interface TextBoxProps {
   placeholder?: string;
   onChange?: (value: string) => void;
   className?: string;
-  type: "search" | "reply";
+  type: "search" | "reply" | "password";
   value?: string;
 }
 
 export const TextBox = ({ placeholder, onChange, className = "", type, value }: TextBoxProps) => {
-  return <div className={`container ${className}`}>{type === "search" ? <input type="text" className={`input input-${type}`} placeholder={placeholder} onChange={(e) => onChange?.(e.target.value)} value={value} /> : <textarea className={`input input-${type}`} placeholder={placeholder} onChange={(e) => onChange?.(e.target.value)} value={value} />}</div>;
+  return (
+    <div className={`container ${className}`}>
+      {type === "search" ? (
+        <input type="text" className={`input input-${type}`} placeholder={placeholder} onChange={(e) => onChange?.(e.target.value)} value={value} />
+      ) : type === "reply" ? (
+        <textarea className={`input input-${type}`} placeholder={placeholder} onChange={(e) => onChange?.(e.target.value)} value={value} />
+      ) : type === "password" ? (
+        <input type="password" className={`input input-${type}`} placeholder={placeholder} onChange={(e) => onChange?.(e.target.value)} value={value} />
+      ) : (
+        ""
+      )}
+    </div>
+  );
 };
---
+++ frontend/src/app/components/loginForm/loginForm.tsx
@@ -0,0 +1,16 @@
+import { TextBox } from "@/app/components/common/textBox/textBox";
+import "@/style/loginForm.scss";
+import { Button } from "../common/button/button";
+
+export const LoginForm = () => {
+  return (
+    <div className="loginForm">
+      <TextBox placeholder="静大メアド" type="search" className="mb-20" />
+      <TextBox placeholder="パスワード" type="password" />
+      <div className="loginForm__button flexWrap">
+        <Button label="新規登録" type="normal" reverse={true} />
+        <Button label="ログイン" type="normal" />
+      </div>
+    </div>
+  );
+};
