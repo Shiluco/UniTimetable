4480daa172967b3a8d0ad69fcb76371e06624931
koto623 <akan.02toko@gmail.com>
2025-01-25 06:57:30 +0900

    feat: homeを作成

---
+++ frontend/src/app/(with-auth)/home/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import styles from "./page.module.css";
+import { Logo } from "@/app/components/common/logo/logo";
+import { NavMenu } from "@/app/components/navMenu/navMenu";
+import { PostItem } from "@/app/components/postItem/postItem";
+import { SearchBox } from "@/app/components/searchBox/searchBox";
+
+export default function HomePage() {
+  return (
+    <div className={styles.gridContainer}>
+      <div className={styles.gridItem}>
+        <Logo className={styles.mb50} />
+        <NavMenu />
+      </div>
+      <div className={`${styles.gridItem} ${styles.scrollable}`}>
+        <PostItem schedules={[]} name="Sample Name" department="Sample Department" major="Sample Major" year={1} className={styles.timeline_postItem} />
+      </div>
+      <div className={styles.gridItem}>
+        <SearchBox departmentOptions={[]} majorOptions={[]} yearOptions={[]} />
+      </div>
+    </div>
+  );
+}
